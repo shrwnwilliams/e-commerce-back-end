1a25720cb66574670fd7b9cdbdc1b5a8cb2d4a35
DINNERman <shrwnwilliams@gmail.com>
2021-04-22 14:57:03 -0400

    finished the api routes for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,12 +29,32 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  ).then((updatedCategory) =>{
+    res.json(updatedCategory);
+  }).catch((err) => {
+    res.json(err);
+  });
 });
 
 router.delete('/:id', async (req, res) => {
