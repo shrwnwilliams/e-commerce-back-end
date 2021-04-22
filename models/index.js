7235961b31e012e40f8a06b17d1215df157cf720
DINNERman <shrwnwilliams@gmail.com>
2021-04-21 22:31:54 -0400

    added all db models and began associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.hasOne(Category, {
+  foreignKey: "category_id"
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.hasMany(Tag,{
+
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.hasMany(Product, {
+  
+})
+
 
 module.exports = {
   Product,
